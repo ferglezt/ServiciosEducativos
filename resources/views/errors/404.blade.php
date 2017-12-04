991c6ee1c73e415986b70b98ba8a88e8ac5bd172
Fernando Roberto González Tapia <fgonzalez@calymayor.com.mx>
2017-12-04 13:21:26 -0600

    Custom error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title', 'Login')
+
+@section('content')
+
+  <img src="{{URL::to('/')}}/images/ipn404.jpg" class="img-responsive" style="margin: auto;"> 
+
+@stop
