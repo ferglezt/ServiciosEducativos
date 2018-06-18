92a950885eaa86d9296a0a062f95d5d7301747de
fernando <fernando.gonzalez.upiicsa@gmail.com>
2018-06-17 19:59:59 -0500

    Alta admins

--- app/Http/Controllers/CapturistaController.php
+++ app/Http/Controllers/CapturistaController.php
@@ -26,6 +26,11 @@ class CapturistaController extends Controller
 
         $usuario = Usuario::findOrFail($id);
 
+        //No se permite cambiarle la contraseña a otro admin
+        if($usuario->rol_id == self::ADMIN) {
+            abort(401);
+        }
+
         $usuario->password = Hash::make($password);
 
         $usuario->save();
@@ -66,8 +71,8 @@ class CapturistaController extends Controller
         }
 
         return view('verCapturistas', [
-            'capturistas' => Usuario::where('rol_id', '!=', self::ADMIN)->get(),
-            'roles' => self::getAllRolesExceptAdmin()
+            'capturistas' => Usuario::all(),
+            'roles' => Rol::all()
         ]);
     }
 
@@ -78,7 +83,7 @@ class CapturistaController extends Controller
     	}
         
         return view('altaCapturista', [
-            'roles' => self::getAllRolesExceptAdmin()
+            'roles' => Rol::all()
         ]);
     }
 
@@ -95,28 +100,28 @@ class CapturistaController extends Controller
 
 		if(!isset($nombre) || $nombre == '') {
 			return view('altaCapturista', [
-                'roles' => self::getAllRolesExceptAdmin(),
+                'roles' => Rol::all(),
                 'error' => 'El campo nombre es obligatorio'
             ]);
     	}
 
 		if(!isset($email) || $email == '') {
 			return view('altaCapturista', [
-                'roles' => self::getAllRolesExceptAdmin(),
+                'roles' => Rol::all(),
                 'error' => 'El campo email es obligatorio']
             );
     	}
 
     	if(!isset($password) || $password == '') {
     		return view('altaCapturista', [
-                'roles' => self::getAllRolesExceptAdmin(),
+                'roles' => Rol::all(),
                 'error' => 'El campo password es obligatorio']
             );
     	}
 
         if(!isset($rol) || $rol == 0) {
             return view('altaCapturista', [
-                'roles' => self::getAllRolesExceptAdmin(),
+                'roles' => Rol::all(),
                 'error' => 'Debe elegir un rol para el usuario']
             );
         }
@@ -131,13 +136,13 @@ class CapturistaController extends Controller
     		$usuario->save();
     	} catch(QueryException $e) {
     		return view('altaCapturista', [
-                'roles' => self::getAllRolesExceptAdmin(),
+                'roles' => Rol::all(),
                 'error' => 'Este usuario ya existe'
             ]);
     	}
 
     	return view('altaCapturista', [
-            'roles' => self::getAllRolesExceptAdmin(),
+            'roles' => Rol::all(),
             'successMessage' => 'Se ha dado de alta al capturista '.$nombre
         ]);
     	
@@ -145,9 +150,5 @@ class CapturistaController extends Controller
 
     private function isAdmin(Request $request) {
     	return $request->session()->get('rol_id', 0) == self::ADMIN;
-    }
-
-    private function getAllRolesExceptAdmin() {
-        return Rol::where('id', '!=', self::ADMIN)->get();
     }
 }
